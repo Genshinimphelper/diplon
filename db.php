749a5e82969e8140320e264a954a76504ee0a490
Андрей <elenets@mcwve>
2026-03-24 19:34:46 +0300

    4

--- db.php
+++ db.php
@@ -1,33 +1,28 @@
 <?php
 $host = "postgresql-diplon.alwaysdata.net";
 $port = "5432";
-$dbname = "diplon_db"; 
+$dbname = "diplon_db";
 $user = "diplon";
-$password = "qazplm1538";
+$password = "qazplm1538"; // Твой пароль
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
-    die("Ошибка подключения к базе данных.");
+    die("Error: Unable to connect to database.");
 }
 
-// 2. БЕЗОПАСНАЯ загрузка настроек (БЕЗ строки на 15 линии)
+// ПРИНУДИТЕЛЬНО УСТАНАВЛИВАЕМ ПУТЬ К ТАБЛИЦАМ
+pg_query($conn, "SET search_path TO public");
+
+// Безопасная загрузка настроек
 $site = [];
-
-// Проверяем существование таблицы, подавляя ошибки через @
-$check_table = @pg_query($conn, "SELECT 1 FROM information_schema.tables WHERE table_name = 'site_settings'");
-
-if ($check_table && pg_num_rows($check_table) > 0) {
-    // Если таблица есть, загружаем данные. 
-    // На всякий случай указываем схему public.site_settings
-    $s_query = pg_query($conn, "SELECT key, value FROM public.site_settings");
-    if ($s_query) {
-        while($s_row = pg_fetch_assoc($s_query)) {
-            $site[$s_row['key']] = $s_row['value'];
-        }
+$res = pg_query($conn, "SELECT key, value FROM site_settings");
+if ($res) {
+    while($row = pg_fetch_assoc($res)) {
+        $site[$row['key']] = $row['value'];
     }
-} else {
-    // Если таблицы нет, задаем значения по умолчанию, чтобы сайт не был пустым
-    $site['site_phone'] = '+7 (000) 000-00-00';
-    $site['site_address'] = 'Адрес не установлен';
 }
+
+// Заглушки, если в базе всё-таки пусто (чтобы сайт не падал)
+if (!isset($site['site_phone'])) $site['site_phone'] = '+7 (900) 123-45-67';
+if (!isset($site['site_address'])) $site['site_address'] = 'Москва, Автомобильная 15';
