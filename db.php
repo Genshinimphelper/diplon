7784b5207c5210a27163ef628bbf4d51a460ce36
Андрей <elenets@mcwve>
2026-03-31 16:48:56 +0300

    4

--- db.php
+++ db.php
@@ -1,28 +1,33 @@
 <?php
+// Данные подключения
 $host = "postgresql-diplon.alwaysdata.net";
 $port = "5432";
 $dbname = "diplon_db";
 $user = "diplon";
-$password = "qazplm1538"; // Твой пароль
+$password = "qazplm1538";
 
+// 1. Подключаемся
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
-    die("Error: Unable to connect to database.");
+    die("Критическая ошибка: Не удалось подключиться к базе данных.");
 }
 
-// ПРИНУДИТЕЛЬНО УСТАНАВЛИВАЕМ ПУТЬ К ТАБЛИЦАМ
+// 2. ПРИНУДИТЕЛЬНО указываем искать в схеме public
+// Это исправит ошибку "relation does not exist"
 pg_query($conn, "SET search_path TO public");
 
-// Безопасная загрузка настроек
+// 3. Загружаем настройки (используем явный префикс public. на всякий случай)
 $site = [];
-$res = pg_query($conn, "SELECT key, value FROM site_settings");
+$res = @pg_query($conn, "SELECT key, value FROM public.site_settings");
+
 if ($res) {
     while($row = pg_fetch_assoc($res)) {
         $site[$row['key']] = $row['value'];
     }
 }
 
-// Заглушки, если в базе всё-таки пусто (чтобы сайт не падал)
-if (!isset($site['site_phone'])) $site['site_phone'] = '+7 (900) 123-45-67';
-if (!isset($site['site_address'])) $site['site_address'] = 'Москва, Автомобильная 15';
+// 4. Заглушки (если база данных всё равно не ответила, сайт не упадет)
+if (empty($site['site_phone'])) $site['site_phone'] = '+7 (900) 123-45-67';
+if (empty($site['site_address'])) $site['site_address'] = 'Москва, Автомобильная 15';
+if (empty($site['credit_rate'])) $site['credit_rate'] = '12';
